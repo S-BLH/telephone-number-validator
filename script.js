3c2c53e46516f0cc65c86a8b8073900792126fe0
sumita <sumitabalouch@gmail.com>
2024-07-02 17:56:17 +0430

    modified the changes in the java script

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-document.getElementById('check-btn').addEventListener('click', function() {
-    var userInput = document.getElementById('user-input').value;
-    var resultsDiv = document.getElementById('results-div');
+document.getElementById('check-btn').addEventListener('click', () => {
+    let userInput = document.getElementById('user-input').value;
+    let resultsDiv = document.getElementById('results-div');
   
     if (userInput === '') {
       alert('Please provide a phone number');
       return;
     }
   
-    var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}$/;
+    let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}$/;
   
     if (regex.test(userInput)) {
-      resultsDiv.innerText = 'Valid US number: ' + userInput;
+      resultsDiv.innerText = `Valid US number: ${userInput}`;
     } else {
-      resultsDiv.innerText = 'Invalid US number: ' + userInput;
+      resultsDiv.innerText = `Invalid US number: ${userInput}`;
     }
   });
   
-  document.getElementById('clear-btn').addEventListener('click', function() {
-    var resultsDiv = document.getElementById('results-div');
+  document.getElementById('clear-btn').addEventListener('click', () => {
+    let resultsDiv = document.getElementById('results-div');
     resultsDiv.innerText = '';
   });
