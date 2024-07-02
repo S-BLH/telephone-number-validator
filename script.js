987094b22dabc3b6ac3d8b9ef137ad090fe3d206
sumita <sumitabalouch@gmail.com>
2024-07-02 17:59:05 +0430

    modified the errors in the code

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.getElementById('check-btn').addEventListener('click', () => {
-    let userInput = document.getElementById('user-input').value;
-    let resultsDiv = document.getElementById('results-div');
+    const userInput = document.getElementById('user-input').value;
+    const resultsDiv = document.getElementById('results-div');
   
     if (userInput === '') {
       alert('Please provide a phone number');
       return;
     }
   
-    let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}$/;
+    const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}$/;
   
     if (regex.test(userInput)) {
       resultsDiv.innerText = `Valid US number: ${userInput}`;
@@ -17,6 +17,6 @@ document.getElementById('check-btn').addEventListener('click', () => {
   });
   
   document.getElementById('clear-btn').addEventListener('click', () => {
-    let resultsDiv = document.getElementById('results-div');
+    const resultsDiv = document.getElementById('results-div');
     resultsDiv.innerText = '';
   });
