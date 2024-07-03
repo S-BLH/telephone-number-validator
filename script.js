aa7a5cca5de4dfc3466caecc543e8501dfb5e2ca
sumita <sumitabalouch@gmail.com>
2024-07-03 14:32:31 +0430

    modified the code errors

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-document.getElementById ('check-btn').addEventListener('click', function() {
+document.getElementById('check-btn').addEventListener ('click', function() {
   const userInput = document.getElementById('user-input').value;
   const resultsDiv = document.getElementById('results-div');
 
   if (userInput === '') {
     ('Please provide a phone number');
     return;
-    }
+  }
 
     let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}$/;
 
   if (regex.test(userInput)) {
     resultsDiv.innerText = 'Valid US number: ' + userInput;
   } else {
-  resultsDiv.innerText = 'Invalid US number: ' + userInput;
+    resultsDiv.innerText = 'Invalid US number: ' + userInput;
   }
 });
 
-  document.getElementById ('clear-btn').addEventListener('click', function() {
+document.getElementById('clear-btn').addEventListener ('click', function() {
   const resultsDiv = document.getElementById('results-div');
   resultsDiv.innerText = '';
 });
